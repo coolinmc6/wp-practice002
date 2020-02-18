daa84892f44368dd2ba358e03daf57662ab6f6d0
Colin McNamara <coolinmc6@gmail.com>
2020-02-18 15:35:33 -0500

    Finished

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import _ from 'lodash';
+import './style.css';
+import Map from './gray-map.gif';
+import Data from './data.xml';
 
 function component() {
   const element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  const map = new Image();
+  map.src = Map;
+
+  element.appendChild(map);
+
+  console.log(Data);
 
   return element;
 }
